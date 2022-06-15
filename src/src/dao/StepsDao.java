61f71e36710e669803265a1cb4837f9a199f42c8
dojo2022-kondo <103239321+dojo2022-kondo@users.noreply.github.com>
2022-06-15 17:07:22 +0900

    StepsDao.javaを更新（createStepメソッドを追加しました）

--- src/src/dao/StepsDao.java
+++ src/src/dao/StepsDao.java
@@ -1,5 +1,59 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class StepsDao {
+
+	//createStepメソッドでステップの新規登録
+	public boolean createStep(String step) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			//SQL文を準備する
+			String sql = "INSERT INTO steps (step, declaration_id) VALUES (?, SELECT  MAX(id) FROM declarations);";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SQL文を完成させる
+			pStmt.setString(1, step);
+
+			//SQL文を実行する（DBに送る）
+
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	//結果を返す
+	return result;
+	}
 
 }

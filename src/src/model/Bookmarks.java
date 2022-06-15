f3775f256b142df02cc8cd9a1a07bc6d609f2618
dojo2022-Hiromachi <103239196+dojo2022-Hiromachi@users.noreply.github.com>
2022-06-15 14:13:49 +0900

    Beansの更新(プロパティの名前変更)

--- src/src/model/Bookmarks.java
+++ src/src/model/Bookmarks.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 
 public class Bookmarks implements Serializable{
 	private int id;//ブックマークID
-	private String user_id;//ユーザーID
-	private int declaration_id;//宣言ID
+	private String userId;//ユーザーID
+	private int declarationId;//宣言ID
 
 	//ゲッター、セッター
 	public int getId() {
@@ -14,18 +14,21 @@ public class Bookmarks implements Serializable{
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getUser_id() {
-		return user_id;
+	public String getUserId() {
+		return userId;
 	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
-	public int getDeclaration_id() {
-		return declaration_id;
+	public int getDeclarationId() {
+		return declarationId;
 	}
-	public void setDeclaration_id(int declaration_id) {
-		this.declaration_id = declaration_id;
+	public void setDeclarationId(int declarationId) {
+		this.declarationId = declarationId;
 	}
+
+
+
 
 
 }
--- src/src/model/Declarations.java
+++ src/src/model/Declarations.java
@@ -5,67 +5,57 @@ public class Declarations implements Serializable {
 	private int id; //宣言ID
 	private String declaration; //宣言
 	private int tag; //タグ
-	private boolean achieve_flag; //達成
-	private boolean private_flag; //非公開
-	private boolean delete_flag; //削除
-	private String user_id; //外部キー
+	private boolean achieveFlag; //達成
+	private boolean privateFlag; //非公開
+	private boolean deleteFlag; //削除
+	private String userId; //外部キー
+
 
 	//ゲッターとセッター
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getDeclaration() {
 		return declaration;
 	}
-
 	public void setDeclaration(String declaration) {
 		this.declaration = declaration;
 	}
-
 	public int getTag() {
 		return tag;
 	}
-
 	public void setTag(int tag) {
 		this.tag = tag;
 	}
-
-	public boolean isAchieve_flag() {
-		return achieve_flag;
+	public boolean isAchieveFlag() {
+		return achieveFlag;
+	}
+	public void setAchieveFlag(boolean achieveFlag) {
+		this.achieveFlag = achieveFlag;
+	}
+	public boolean isPrivateFlag() {
+		return privateFlag;
+	}
+	public void setPrivateFlag(boolean privateFlag) {
+		this.privateFlag = privateFlag;
+	}
+	public boolean isDeleteFlag() {
+		return deleteFlag;
+	}
+	public void setDeleteFlag(boolean deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
-	public void setAchieve_flag(boolean achieve_flag) {
-		this.achieve_flag = achieve_flag;
-	}
 
-	public boolean isPrivate_flag() {
-		return private_flag;
-	}
-
-	public void setPrivate_flag(boolean private_flag) {
-		this.private_flag = private_flag;
-	}
-
-	public boolean isDelete_flag() {
-		return delete_flag;
-	}
-
-	public void setDelete_flag(boolean delete_flag) {
-		this.delete_flag = delete_flag;
-	}
-
-	public String getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
 }
 
 

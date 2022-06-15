9deaca590f7d197662a558acf2dc1f6e64c4bb0b
dojo2022-Hiromachi <103239196+dojo2022-Hiromachi@users.noreply.github.com>
2022-06-15 13:27:49 +0900

    Reactions.javaを更新(beansを作成)

--- src/src/model/Reactions.java
+++ src/src/model/Reactions.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Reactions {
+	private int id;
+	private String users_id;
+	private int declarations_id;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsers_id() {
+		return users_id;
+	}
+	public void setUsers_id(String users_id) {
+		this.users_id = users_id;
+	}
+	public int getDeclarations_id() {
+		return declarations_id;
+	}
+	public void setDeclarations_id(int declarations_id) {
+		this.declarations_id = declarations_id;
+	}
+
 
 }

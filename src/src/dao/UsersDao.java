f7f1eb62cbf754dac1a5fe35dfee7b9fd7c8512b
dojo2022-NAKAHARA <NAKAHARA-AYANE-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-16 14:18:30 +0900

    UsersDao.javaの更新（メゾット追加）

--- src/src/dao/UsersDao.java
+++ src/src/dao/UsersDao.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Users;
 
 public class UsersDao {
 
@@ -65,6 +69,66 @@ public class UsersDao {
 
 // 結果を返す
 		return loginResult;
+	}
+
+//セッションスコープから取得したユーザーIDを引数にして、ユーザー情報を持ってくる
+//	ユーザーの画面表示
+
+	public List<Users> selectUsers(String id) {
+		Connection conn = null;
+		ArrayList<Users> usersList = new ArrayList<Users>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT id, name, comment, icon, themecolor FROM users WHERE id = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+
+			// SQL文を完成させる
+			pStmt.setString(1,id);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Users user = new Users();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setComment(rs.getString("comment"));
+				user.setIcon(rs.getInt("icon"));
+				user.setThemecolor(rs.getInt("themecolor"));
+				usersList.add(user);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			usersList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			usersList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					usersList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return usersList;
 	}
 }
 

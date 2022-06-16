6b8e176e3084219e192516ee8406a1114a9a1bb2
dojo2022-fujii <103237661+dojo2022-fujii@users.noreply.github.com>
2022-06-16 14:27:37 +0900

    UsersDao.javaの更新（UpdateUsersメソッド）

--- src/src/dao/UsersDao.java
+++ src/src/dao/UsersDao.java
@@ -71,6 +71,57 @@ public class UsersDao {
 		return loginResult;
 	}
 
+
+
+//Usersテーブルを更新するメソッド
+		public boolean updateUser(String name, String comment, int icon, int themecolor) {
+			Connection conn = null;
+				boolean updateResult =false;
+
+			//以下の処理が成功した場合、updateResult =true（更新成功）
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+				// SELECT文を準備する
+				String sql = "UPDATE users SET name=?, comment=?, icon=?, themecolor=? WHERE id=?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				pStmt.setString(1, name);
+				pStmt.setString(2, comment);
+				pStmt.setInt(3, icon);
+				pStmt.setInt(4, themecolor);
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					updateResult = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			// 結果を返す
+			return updateResult;
+	}
+
+
 //セッションスコープから取得したユーザーIDを引数にして、ユーザー情報を持ってくる
 //	ユーザーの画面表示
 

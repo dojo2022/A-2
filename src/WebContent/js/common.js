a45634dad546338adee56047737605c9a0cd4c1c
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-16 17:13:32 +0900

    common.jsの更新(toggleNavファンクションの追加))

--- src/WebContent/js/common.js
+++ src/WebContent/js/common.js
@@ -1,3 +1,23 @@
-/**
- * 
- */
+
+'use strict'
+
+$(function() {
+  function toggleNav() {
+    let body = document.body;
+    let hamburger = document.getElementById("js-hamburger");
+    let cancel = document.getElementById("cancel");
+
+    // クリックしたときにクラス付与
+    hamburger.addEventListener("click", function() {
+      body.classList.toggle("nav-open");
+      hamburger.classList.toggle("active");
+    });
+
+    // クリックしたときにクラス付与
+    blackBg.addEventListener("click", function() {
+      body.classList.remove("nav-open");
+      hamburger.classList.remove("active");
+    });
+  }
+  toggleNav();
+});

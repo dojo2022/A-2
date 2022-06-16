cb0aa8ba08d75f4568b9587420393b22db57372b
dojo2022-kondo <103239321+dojo2022-kondo@users.noreply.github.com>
2022-06-16 11:14:55 +0900

    StepsDao.javaを更新（３つのメソッド追加、完成）

--- src/src/dao/StepsDao.java
+++ src/src/dao/StepsDao.java
@@ -56,4 +56,157 @@ public class StepsDao {
 	return result;
 	}
 
+
+	//editDecメソッドでステップを編集する
+	public boolean editStep(String step, int id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			//SQL文を準備する
+			String sql = "UPDATE steps SET step = ? WHERE id =?;";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SQL文を完成させる
+			pStmt.setString(1, step);
+			pStmt.setInt(2, id);
+
+			//SQL文を実行する（DBに送る）
+
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	//結果を返す
+	return result;
+
+	}
+
+
+	//deleteStepメソッドでステップを削除する
+	public boolean deleteStep(int id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			//SQL文を準備する
+			String sql = "DELETE * FROM steps WHERE id = ?;";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SQL文を完成させる
+			pStmt.setInt(1, id);
+
+			//SQL文を実行する（DBに送る）
+
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	//結果を返す
+	return result;
+
+	}
+
+
+	//achieveStepメソッドで達成フラグを０→１へ
+	public boolean achieveStep(int id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			//SQL文を準備する
+			String sql = "UPDATE steps SET achieve_flag = 1 WHERE id = ?;";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SQL文を完成させる
+			pStmt.setInt(1, id);
+
+			//SQL文を実行する（DBに送る）
+
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	//結果を返す
+	return result;
+
+	}
 }

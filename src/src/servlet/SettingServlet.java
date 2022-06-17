6cb8d2ed7e84b8fb71da5206df2163e6dda9d260
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-18 00:27:41 +0900

    SettingServlet.javaの更新(doPostの更新)

--- src/src/servlet/SettingServlet.java
+++ src/src/servlet/SettingServlet.java
@@ -22,7 +22,7 @@ public class SettingServlet extends HttpServlet {
 		/*
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/simpleBC/LoginServlet");
+			response.sendRedirect("/KillerQueen/LoginServlet");
 			return;
 		}
 		*/
@@ -35,14 +35,12 @@ public class SettingServlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		/*
+	// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/simpleBC/LoginServlet");
+			response.sendRedirect("/KillerQueen/LoginServlet");
 			return;
 		}
-		*/
 
 	// リクエストスコープ(parameter区画)から「ユーザー名」「一言」「アイコン」「テーマカラー」を取得
 		request.setCharacterEncoding("UTF-8");
@@ -52,46 +50,58 @@ public class SettingServlet extends HttpServlet {
 		name = name.replaceAll("　", ""); // 全角スペースを空文字に置換
         name = name.replaceAll(" ", ""); // 半角スペースを空文字に変換
 
-		String comment = request.getParameter("comment");
-		int icon = Integer.parseInt(request.getParameter("icon"));
-		int themecolor  = Integer.parseInt(request.getParameter("themecolor"));
-
-
-	// UsersDaoをインスタンス化して更新処理を行う
-		// UsersDaoをインスタンス化
-		UsersDao usersDao = new UsersDao();
-
-	// 更新処理
-		boolean ans = usersDao.updateUser(name, comment, icon, themecolor);
-
-		if (ans == true) {
-			// セッションスコープ(attribute区画)にidを格納する
-			HttpSession session = request.getSession();
-			session.setAttribute("name", name);
-			session.setAttribute("comment", comment);
-			session.setAttribute("icon", icon);
-			session.setAttribute("themecolor", themecolor);
-
-			// MyPageServletにリダイレクトする
-			response.sendRedirect("/killerQueen/MyPageServlet");
-		}
-
-		// ログイン失敗(UsersDaoかfalseが返ってきた場合)
-		else {
+        if (name == "") {
 			// リクエストスコープ(attribute区画)にエラーメッセージを格納する
 			Result result = new Result();
-			result.setMessage("");
+			result.setMessage("スペースは無効です。");
 			request.setAttribute("result", result);
 
-			// ログイン画面にフォワードする
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			// 設定画面にフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/setting.jsp");
 			dispatcher.forward(request, response);
-		}
+
+        } else {
+			String comment = request.getParameter("comment");
+			int icon = Integer.parseInt(request.getParameter("icon"));
+			int themecolor  = Integer.parseInt(request.getParameter("themecolor"));
+			String id = (String)session.getAttribute("id");
+
+		// UsersDaoをインスタンス化して変更処理を行う
+			// UsersDaoをインスタンス化
+			UsersDao usersDao = new UsersDao();
+
+		// 変更処理
+			boolean ans = usersDao.updateUser(name, comment, icon, themecolor, id);
 
 
+			if (ans == true) {
 
+				// もともとのセッションスコープを破棄
+				session.invalidate();
 
+				// セッションスコープ(attribute区画)にidを格納する
+				session.setAttribute("name", name);
+				session.setAttribute("comment", comment);
+				session.setAttribute("icon", icon);
+				session.setAttribute("themecolor", themecolor);
 
+				// MyPageServletにリダイレクトする
+				response.sendRedirect("/killerQueen/MyPageServlet");
+			}
+
+			// 変更失敗(UsersDaoかfalseが返ってきた場合)
+			else {
+
+				// リクエストスコープ(attribute区画)にエラーメッセージを格納する
+				Result result = new Result();
+				result.setMessage("変更できませんでした。");
+				request.setAttribute("result", result);
+
+				// 設定画面にフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/setting.jsp");
+				dispatcher.forward(request, response);
+			}
+        }
 
 	}
 

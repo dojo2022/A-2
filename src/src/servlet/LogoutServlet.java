04b1e393f99601156e39f1e4201771d4be864ee1
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-17 13:37:46 +0900

    LogoutServlet.javaの更新(doPostの削除)

--- src/src/servlet/LogoutServlet.java
+++ src/src/servlet/LogoutServlet.java
@@ -9,16 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LogoutServlet
- */
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// セッションスコープを破棄する
 		HttpSession session = request.getSession();
@@ -27,13 +21,4 @@ public class LogoutServlet extends HttpServlet {
 		// ログインページにリダイレクトする
 		response.sendRedirect("/killerQueen/LoginServlet");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }

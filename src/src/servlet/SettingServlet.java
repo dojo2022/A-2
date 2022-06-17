8ef63d241855678bb78f88a1912ee2c3224f7cea
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-17 15:25:50 +0900

    SettingServlet.javaの更新(doPostの更新)

--- src/src/servlet/SettingServlet.java
+++ src/src/servlet/SettingServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UsersDao;
+import model.Result;
 
 @WebServlet("/SettingServlet")
 public class SettingServlet extends HttpServlet {
@@ -15,11 +19,13 @@ public class SettingServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		/* HttpSession session = request.getSession();
+		/*
+		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
 			response.sendRedirect("/simpleBC/LoginServlet");
 			return;
-		} */
+		}
+		*/
 
 		// 設定画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/setting.jsp");
@@ -29,7 +35,64 @@ public class SettingServlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		/*
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/simpleBC/LoginServlet");
+			return;
+		}
+		*/
+
+	// リクエストスコープ(parameter区画)から「ユーザー名」「一言」「アイコン」「テーマカラー」を取得
+		request.setCharacterEncoding("UTF-8");
+
+		String name = request.getParameter("name");
+		// スペースが入力された場合は空文字にする
+		name = name.replaceAll("　", ""); // 全角スペースを空文字に置換
+        name = name.replaceAll(" ", ""); // 半角スペースを空文字に変換
+
+		String comment = request.getParameter("comment");
+		int icon = Integer.parseInt(request.getParameter("icon"));
+		int themecolor  = Integer.parseInt(request.getParameter("themecolor"));
+
+
+	// UsersDaoをインスタンス化して更新処理を行う
+		// UsersDaoをインスタンス化
+		UsersDao usersDao = new UsersDao();
+
+	// 更新処理
+		boolean ans = usersDao.updateUser(name, comment, icon, themecolor);
+
+		if (ans == true) {
+			// セッションスコープ(attribute区画)にidを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("name", name);
+			session.setAttribute("comment", comment);
+			session.setAttribute("icon", icon);
+			session.setAttribute("themecolor", themecolor);
+
+			// MyPageServletにリダイレクトする
+			response.sendRedirect("/killerQueen/MyPageServlet");
+		}
+
+		// ログイン失敗(UsersDaoかfalseが返ってきた場合)
+		else {
+			// リクエストスコープ(attribute区画)にエラーメッセージを格納する
+			Result result = new Result();
+			result.setMessage("");
+			request.setAttribute("result", result);
+
+			// ログイン画面にフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+
+
+
+
+
+
 	}
 
 }

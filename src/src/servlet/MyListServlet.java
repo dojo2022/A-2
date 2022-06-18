cbdecc7d2fd735d0e56e5d2514235b8d207c6dc6
dojo2022-kondo <103239321+dojo2022-kondo@users.noreply.github.com>
2022-06-18 23:15:38 +0900

    MyListServlet.javaを更新（if文で分岐の追加）

--- src/src/servlet/MyListServlet.java
+++ src/src/servlet/MyListServlet.java
@@ -92,68 +92,74 @@ public class MyListServlet extends HttpServlet {
 			return;
 		}
 
+		String button = request.getParameter("bt");
 		//行われた処理によって①～④条件分岐
+		if(button.equals("新規登録")) {
+			//①新規作成
 
-		//①新規作成
+			//セッションスコープの取得
+			String userId = (String) session.getAttribute("id");
+			// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String declaration = request.getParameter("declarations.id");
+			int tag = Integer.parseInt(request.getParameter("tag"));
+			int privateFlag = Integer.parseInt(request.getParameter("private_flag"));
 
-		//セッションスコープの取得
-		String userId = (String) session.getAttribute("id");
-		// リクエストパラメータを取得する
-		request.setCharacterEncoding("UTF-8");
-		String declaration = request.getParameter("declarations.id");
-		int tag = Integer.parseInt(request.getParameter("tag"));
-		int privateFlag = Integer.parseInt(request.getParameter("private_flag"));
+			String step = request.getParameter("step");
 
-		String step = request.getParameter("step");
+			//DeclarationsDaoのinsertDecメソッドを呼ぶ
+			DeclarationsDao decDao = new DeclarationsDao();
+			boolean decResult = decDao.insertDec(declaration, tag, privateFlag, userId);
+			//StepをINSERTするメゾット
+			StepsDao stepsDao = new StepsDao();
+			boolean stepResult = stepsDao.createStep(step);
 
-		//DeclarationsDaoのinsertDecメソッドを呼ぶ
-		DeclarationsDao decDao = new DeclarationsDao();
-		boolean decResult = decDao.insertDec(declaration, tag, privateFlag, userId);
-		//StepをINSERTするメゾット
-		StepsDao stepsDao = new StepsDao();
-		boolean stepResult = stepsDao.createStep(step);
+			//MyListservletでフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/killerQueen/MyListServlet");
+			dispatcher.forward(request, response);
 
-		//MyListservletでフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/killerQueen/MyListServlet");
-		dispatcher.forward(request, response);
-
-		/*String id = (String) session.getAttribute("id");
-		DeclarationsDao decDao2 = new DeclarationsDao();
-		//引数をidとしてdecDAOのmypagedecメゾット呼び出し
-		ArrayList<CommonTable> pageList = decDao2.myListDec(id);
-		*/
-
+			/*String id = (String) session.getAttribute("id");
+			DeclarationsDao decDao2 = new DeclarationsDao();
+			//引数をidとしてdecDAOのmypagedecメゾット呼び出し
+			ArrayList<CommonTable> pageList = decDao2.myListDec(id);
+			*/
+		}
 		//②編集
 		// リクエストパラメータを取得する
+		if(button.equals("編集")) {
+			//選択されたデータを受け取る
+			String userId2 = (String) session.getAttribute("id");
+			request.setCharacterEncoding("UTF-8");
+			String declaration2 = request.getParameter("declaration");
+			int tag2 = Integer.parseInt(request.getParameter("tag"));
+			int privateFlag2 = Integer.parseInt(request.getParameter("private_flag"));
 
-		//選択されたデータを受け取る
-		String userId2 = (String) session.getAttribute("id");
-		request.setCharacterEncoding("UTF-8");
-		String declaration2 = request.getParameter("declaration");
-		int tag2 = Integer.parseInt(request.getParameter("tag"));
-		int privateFlag2 = Integer.parseInt(request.getParameter("private_flag"));
-
-		int decId= Integer.parseInt(request.getParameter("declaration_id"));
+			int decId= Integer.parseInt(request.getParameter("declaration_id"));
 
 
-		String step2 = request.getParameter("step");
+			String step2 = request.getParameter("step");
 
-		//データをDeclarationsDao.javaにもっていき、データベースにアクセスする
-		//		宣言を編集する処理
-		DeclarationsDao decDao2 = new DeclarationsDao();
-		boolean decResult2 = decDao2.editDec(declaration2, tag2, privateFlag2, userId2);
-		//		ステップを編集する処理
-		StepsDao stepsDao2 = new StepsDao();
-		boolean stepResult2 = stepsDao2.editStep(step,decId);
+			//データをDeclarationsDao.javaにもっていき、データベースにアクセスする
+			//		宣言を編集する処理
+			DeclarationsDao decDao2 = new DeclarationsDao();
+			boolean decResult2 = decDao2.editDec(declaration2, tag2, privateFlag2, userId2);
+			//		ステップを編集する処理
+			StepsDao stepsDao2 = new StepsDao();
+			boolean stepResult2 = stepsDao2.editStep(step2,decId);
 
-		//MyListservletでフォワードする
-		RequestDispatcher dispatcher2 = request.getRequestDispatcher("/killerQueen/MyListServlet");
-		dispatcher2.forward(request, response);
-
+			//MyListservletでフォワードする
+			RequestDispatcher dispatcher2 = request.getRequestDispatcher("/killerQueen/MyListServlet");
+			dispatcher2.forward(request, response);
+		}
 		//③達成
+		if(button.equals("達成")) {
 
+		}
 
 		//④削除
+		if(button.equals("削除")) {
+
+		}
 
 	}
 

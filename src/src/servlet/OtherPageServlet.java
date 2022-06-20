0f437749f767b7d8dce55132f96d009792499d87
dojo2022-ishida <103237674+dojo2022-ishida@users.noreply.github.com>
2022-06-20 12:01:06 +0900

    OtherPageServletの更新

--- src/src/servlet/OtherPageServlet.java
+++ src/src/servlet/OtherPageServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.DeclarationsDao;
+import dao.UsersDao;
+import model.CommonTable;
+import model.Steps;
 
 /**
  * Servlet implementation class OtherPageServlet
@@ -21,6 +28,69 @@ public class OtherPageServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		/*もしもログインしていなかったらログインサーブレットにリダイレクトする
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/killerQueen/loginServlet");
+			return;
+		}*/
+
+		//セッションを使えるようにする
+		HttpSession session = request.getSession();
+		//セッションスコープから自分のID取得
+		String mmid = (String)session.getAttribute("id");
+
+		//リクエストスコープからユーザーIDを取得
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+
+
+
+//テスト用
+		String mytest = "2";
+		String othertest = "1";
+
+		//ユーザーDaoのインスタンスを作成
+		UsersDao user = new UsersDao();
+
+
+
+		//宣言DAOをインスタンス
+		DeclarationsDao decdao = new DeclarationsDao();
+
+		//ArrayList<CommonTable>のインスタンスを作成
+		ArrayList<CommonTable> OtherPageList = decdao.otherPageDec(mytest, othertest);
+
+		//timelineListからstep項目を抽出する
+		ArrayList<Steps> steper = new ArrayList<Steps>();
+		for(int i = 0; i < OtherPageList.size(); i++) {
+			CommonTable ct = OtherPageList.get(i);
+			Steps st = new Steps();
+			st.setStep(ct.getStepsStep());
+			st.setAchieveFlag(ct.isStepsAchieveFlag());
+			st.setDeclarationId(ct.getDecsId());
+			steper.add(st);
+		}
+
+		//timelineListから（DECIDがかぶっている箇所）不要な宣言を削除する
+		int i = 0;
+		int k = 1;
+		while(i < OtherPageList.size()) { //リストが０行以上だったら
+			while(k < OtherPageList.size()) { //リストが１行以上
+				//マイリストのi行目とi+1行目のDecsidが同じだったら行削除、違えばプラス１
+				if(OtherPageList.get(i).getDecsId() == OtherPageList.get(k).getDecsId()) {
+					OtherPageList.remove(k);
+				}else {
+					i++;
+					k++;
+				}
+			}
+			break;
+		}
+
+
+		//リクエストスコープにlistを"OtherPageList"という名前を付けて入れる
+		request.setAttribute("OtherPageList", OtherPageList);
+		request.setAttribute("steper", steper);
 		//others.jspにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/other_page.jsp");
 		dispatcher.forward(request, response);

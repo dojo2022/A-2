91dcc400dd889225d71ca6cbfefb0c1e1595036e
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-20 17:24:15 +0900

    RegistServlet.javaの更新(Ajax以外は完成)

--- src/src/servlet/RegistServlet.java
+++ src/src/servlet/RegistServlet.java
@@ -9,19 +9,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UsersDao;
+
 @WebServlet("/RegistServlet")
 public class RegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 新規登録画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
 		dispatcher.forward(request, response);
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+		response.setHeader("Cache-Control", "nocache");
+		response.setCharacterEncoding("utf-8");
 
+		// 送信されたデータの取得
+		String name = request.getParameter("data1");
+		String id = request.getParameter("data2");
+		String pw = request.getParameter("data3");
+
+		// UsersDaoをインスタンス化して登録処理を行う
+		// UsersDaoをインスタンス化
+		UsersDao usersDao = new UsersDao();
+
+		// 登録処理(isRegistOKメソッド)
+		boolean ans = usersDao.isRegistOk(id, pw, name);
+
+		// 登録成功(UsersDaoからtrueが返ってきた場合)
+		if (ans == true) {
+			String data = "true";
+
+		}
+	}
 }

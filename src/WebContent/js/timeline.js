2372c2c8758019bbdefd319cb4611ec827f4994c
dojo2022-Hiromachi <103239196+dojo2022-Hiromachi@users.noreply.github.com>
2022-06-22 12:02:18 +0900

    timeline.jsの更新

--- src/WebContent/js/timeline.js
+++ src/WebContent/js/timeline.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function goAjax(indexNo){
-			alert("functionはいったよ！");
+			alert("gojaxはいったよ！");
 			//入力値を取得してくる
 			let usersId = 1;
 			let decsId = document.getElementById('decsId' + indexNo).value;
@@ -31,23 +31,83 @@ function goAjax(indexNo){
             .done(function(data) {
             	//alert("成功");
 
-				if(data === "inReactTrue"){
+				if(data == "inReactTrue"){
 				$("#change" + indexNo).html("");
-				//document.getElementById('reaction' + indexNo).innerHTML = `<input type="image" src="/killerQueen/img/icon/redHeart.png" width="30px" value="リアクションしてる" id="reaction${indexNo}" onclick="goAjax('${indexNo}')">`;
 				var htmltext = "";
 				htmltext = htmltext + `<input type="image" src="/killerQueen/img/icon/redHeart.png" width="30px" value="リアクションしてる" id="reaction` + indexNo;
 				htmltext = htmltext + `" onclick="goAjax('` + indexNo;
 				htmltext = htmltext + `')">`;
 				$("#change" + indexNo).append(htmltext);
 
-				}else if(data === "delReactTrue"){
+				}else if(data == "delReactTrue"){
+
 				$("#change" + indexNo).html("");
-				//document.getElementById('reaction' + indexNo).innerHTML = `<input type="image" src="/killerQueen/img/icon/whiteHeart.png" width="30px" value="リアクションしてない" id="reaction${indexNo}" onclick="goAjax("${indexNo}")">`;
 				var htmltext = "";
-				htmltext = htmltext + `<input type="image" src="/killerQueen/img/icon/whiteHeart.png" width="30px" value="リアクションしてる" id="reaction` + indexNo;
+				htmltext = htmltext + `<input type="image" src="/killerQueen/img/icon/whiteHeart.png" width="30px" value="リアクションしてない" id="reaction` + indexNo;
 				htmltext = htmltext + `" onclick="goAjax('` + indexNo;
 				htmltext = htmltext + `')">`;
 				$("#change" + indexNo).append(htmltext);
+
+				}else {
+				document.getElementById("test" + indexNo).innerText = "失敗";
+				}
+			  })
+			   //非同期通信が失敗したときの処理
+			  .fail(function() {
+				//失敗とアラートを出す
+				alert("失敗！");
+			  });
+		}
+
+
+function goBook(indexNo){
+			alert("gobookはいったよ！");
+			//入力値を取得してくる
+			let usersId = 1;
+			let decsId = document.getElementById('decsId' + indexNo).value;
+			let bookStatus = document.getElementById('bookmark' + indexNo).value;
+
+			//{変数名：中に入れるもの}みたいに書いて、複数の値をpostData変数に格納
+			let postData = {data1:decsId,data2:usersId,data3:bookStatus}
+
+
+			//非同期通信始めるよ
+			$.ajaxSetup({scriptCharset:'utf-8'});
+			$.ajax({
+				//どのサーブレットに送るか
+				//ajaxSampleのところは自分のプロジェクト名に変更する必要あり。
+				url: '/killerQueen/TimelineServlet',
+				//どのメソッドを使用するか
+				type:"POST",
+				//ここは今回は決まりの書き方　→　受け取る型
+				dataType:"text",
+				//何をサーブレットに飛ばすか（変数を記述）
+				data: postData,
+				//この下の２行はとりあえず書いてる（書かなくても大丈夫？）
+				processDate:false,
+				timeStamp: new Date().getTime()
+			   //非同期通信が成功したときの処理
+			})
+            .done(function(data) {
+            	//alert("成功");
+
+				if(data == "inBookTrue"){
+
+				$("#changeBook" + indexNo).html("");
+				var htmltext = "";
+				htmltext = htmltext + `<input type="image" src="/killerQueen/img/icon/bookmarkBlack.png" width="30px" value="ブックマークしてる" id="bookmark` + indexNo;
+				htmltext = htmltext + `" onclick="goBook('` + indexNo;
+				htmltext = htmltext + `')">`;
+				$("#changeBook" + indexNo).append(htmltext);
+
+				}else if(data == "delBookTrue"){
+
+				$("#changeBook" + indexNo).html("");
+				var htmltext = "";
+				htmltext = htmltext + `<input type="image" src="/killerQueen/img/icon/bookmarkWhite.png" width="30px" value="ブックマークしてない" id="bookmark` + indexNo;
+				htmltext = htmltext + `" onclick="goBook('` + indexNo;
+				htmltext = htmltext + `')">`;
+				$("#changeBook" + indexNo).append(htmltext);
 
 				}else {
 				document.getElementById("test" + indexNo).innerText = "失敗";

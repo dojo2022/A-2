ccaa574491f65ce9baadb291d35141f992bfc970
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-26 21:50:39 +0900

    コメント追加

--- src/WebContent/js/common.js
+++ src/WebContent/js/common.js
@@ -23,12 +23,18 @@ function changeTheme4(){
 
 
 // ハンバーガーメニュー
+// 三本線をクリックすると行われる処理
 document.querySelector('.line-list').addEventListener('click', function(){
+
+	// 各クラスに新しいクラスが追加されるよ
 	document.querySelector('.global-nav').classList.toggle('active');
 	document.querySelector('.line-list').classList.toggle('active');
 	document.querySelector('.menu-bg').classList.toggle('black');
 
+	// 黒背景をクリックすると行われる処理
 	document.querySelector('.menu-bg').addEventListener('click', function(){
+
+	// 各クラスから追加されたクラスが消えるよ
 	document.querySelector('.global-nav').classList.remove('active');
 	document.querySelector('.line-list').classList.remove('active');
 	document.querySelector('.menu-bg').classList.remove('black');

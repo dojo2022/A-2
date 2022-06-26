d1129bf31476a3613edecbac1f7b676147f2de5f
dojo2022-murakami <103238367+dojo2022-murakami@users.noreply.github.com>
2022-06-26 21:47:39 +0900

    common.jsの更新(黒背景押してもメニュー消えるよ)

--- src/WebContent/js/common.js
+++ src/WebContent/js/common.js
@@ -1,6 +1,5 @@
 
 
-
 // テーマカラーの切り替え
 function changeTheme0() {
       document.documentElement.setAttribute('theme', '0');
@@ -23,35 +22,18 @@ function changeTheme4(){
 }
 
 
-/*
-// ハンバーガーメニューの作り方
-$(function() {
-  function toggleNav() {
-    var body = document.body;
-    var hamburger = document.getElementById("js-hamburger");
-    var blackBg = document.getElementById("js-menu-bg");
-    // クリックしたときにクラス付与
-    hamburger.addEventListener("click", function() {
-      body.classList.toggle("menu-open");
-      hamburger.classList.toggle("active");
-    });
-    // クリックしたときにクラス付与
-    blackBg.addEventListener("click", function() {
-      body.classList.remove("menu-open");
-      hamburger.classList.remove("active");
-    });
-  }
-  toggleNav();
-});
-*/
-
-// ハンバーガーメニュー新
+// ハンバーガーメニュー
 document.querySelector('.line-list').addEventListener('click', function(){
-   document.querySelector('.global-nav').classList.toggle('active');
-   document.querySelector('.line-list').classList.toggle('active');
-   document.querySelector('.menu-bg').classList.toggle('black');
-});
+	document.querySelector('.global-nav').classList.toggle('active');
+	document.querySelector('.line-list').classList.toggle('active');
+	document.querySelector('.menu-bg').classList.toggle('black');
 
+	document.querySelector('.menu-bg').addEventListener('click', function(){
+	document.querySelector('.global-nav').classList.remove('active');
+	document.querySelector('.line-list').classList.remove('active');
+	document.querySelector('.menu-bg').classList.remove('black');
+	});
+});
 
 
 
